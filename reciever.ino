974330d1b730ac7edcd368b62ad4d2297101d3b4
braib <bhavishraib@gmail.com>
2024-02-05 17:47:51 +0530

    basic_sender_and_reciever

---
+++ reciever.ino
@@ -0,0 +1,99 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-many-to-one-esp32/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Structure example to receive data
+// Must match the sender structure
+//typedef struct struct_message {
+//  int id;
+//  int x;
+//  int y;
+//}struct_message;
+
+
+
+
+typedef struct struct_message {
+  int id ;
+  char a[32];
+  int b;
+  float c;
+  String d;
+  bool e;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// Create a structure to hold the readings from each board
+struct_message board1;
+struct_message board2;
+struct_message board3;
+
+// Create an array with all the structures
+struct_message boardsStruct[2] = {board1, board2};
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+//  boardsStruct[myData.id-1].a = myData.a;
+  boardsStruct[myData.id-1].b = myData.b;
+  boardsStruct[myData.id-1].c = myData.c;
+//  boardsStruct[myData.id-1].d = myData.d;
+  boardsStruct[myData.id-1].e = myData.e;
+//  boardsStruct[myData.id-1].b = myData.b;
+  Serial.printf("a value: %c \n", boardsStruct[myData.id-1].a);
+  Serial.printf("b value: %d \n", boardsStruct[myData.id-1].b);
+  Serial.printf("c value: %f \n", boardsStruct[myData.id-1].c);
+//  Serial.printf("d value: %s \n", boardsStruct[myData.id-1].d);
+//  Serial.printf("e value: %d \n", boardsStruct[myData.id-1].e);
+//  Serial.printf("y value: %d \n", boardsStruct[myData.id-1].y);
+  Serial.println();
+}
+ 
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+ 
+void loop() {
+  // Acess the variables for each board
+  /*int board1X = boardsStruct[0].x;
+  int board1Y = boardsStruct[0].y;
+  int board2X = boardsStruct[1].x;
+  int board2Y = boardsStruct[1].y;
+  int board3X = boardsStruct[2].x;
+  int board3Y = boardsStruct[2].y;*/
+
+  delay(10000);  
+}
